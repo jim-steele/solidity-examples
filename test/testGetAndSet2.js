9ba117a73657c2c07108ce94d121ec036e1cbd25
Jim <jim-steele@users.noreply.github.com>
2018-05-29 18:03:54 -0700

    Rewrote unit test script with more readable async/await syntax.

--- test/testGetAndSet2.js
+++ test/testGetAndSet2.js
@@ -1,29 +1,21 @@
-const GetAndSet2 = artifacts.require("./GetAndSet2.sol");
+var GetAndSet2 = artifacts.require("./GetAndSet2.sol");
 
-contract('GetAndSet2TestJavascript', function(accounts) {
+contract('GetAndSet2TestAsync', async (accounts) => {
 
-	it("should be zero if unset", function() {
-
-		return GetAndSet2.deployed().then(function(instance) {
-			return instance.getStoredData.call(1);
-		    }).then(function(value) {
-			    assert.equal(value.valueOf(), 0, "Unset parameter was not zero");
-			});
+	it("should be zero if unset", async () => {
+		let instance = await GetAndSet2.deployed();
+		let value = await instance.getStoredData(1);
+		assert.equal(value.valueOf(), 0, "Unset parameter was not zero");
 	    });
 
-	it("should store the specified value", function() {
-		var gns2Inst;
+	it("should store the specified value", async () => {
 		var specifiedIdx = 0;
 		var specifiedVal = 'howdy';
 
-		return GetAndSet2.deployed().then(function(instance) {
-			gns2Inst = instance;
-			return gns2Inst.setStoredData(specifiedIdx, specifiedVal, {from: accounts[0]});
-		    }).then(function() {
-			    return gns2Inst.getStoredData.call(specifiedIdx);
-			}).then(function(storedData) {
-				assert.equal(storedData, specifiedVal, "Set parameter was not returned");
-			    });
+		let instance = await GetAndSet2.deployed();
+		await instance.setStoredData(specifiedIdx, specifiedVal);
+		let storedData = await instance.getStoredData(specifiedIdx);
+		assert.equal(storedData, specifiedVal, "Set parameter was not returned");
 	    });
 
     });
